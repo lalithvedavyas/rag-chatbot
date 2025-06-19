0295e244a39521b63f87f23718513a9794d1b105
lalithvedavyas <lalithvedavyas09@gmail.com>
2025-06-19 18:38:16 +0000

    application

--- app/ingest.py
+++ app/ingest.py
@@ -1,5 +1,5 @@
 from langchain_community.document_loaders import DirectoryLoader
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
